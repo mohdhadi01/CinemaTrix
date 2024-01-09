5b05e39785942f8c68b6e2ab4087f284a793ef7b
mohdhadi01 <hadi.88923@gmail.com>
2024-01-09 16:37:48 +0530

    #completed building Signup and Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './Pages/Footer';
-import Header from './Pages/Header';
+import Login from './Login & Signup/Login';
+import SignUp from './Login & Signup/SignUp';
+import Home from './Pages/Home';
 
 function App() {
+  // const APIKey="a122cee36b1bc254ee171ee36a29bb98";
+
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTIyY2VlMzZiMWJjMjU0ZWUxNzFlZTM2YTI5YmI5OCIsInN1YiI6IjY1MjgyY2E5MWYzZTYwMDEzOTlkMjg2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AHxTYhk6c-0aPLETLVS2-oxQqETUReQ5Uyhi9-eZ5bA'
+  //   }
+  // };
+  
+  // fetch('https://api.themoviedb.org/3/configuration', options)
+  //   .then(response => response.json())
+  //   .then(response => console.log(response))
+  //   .catch(err => console.error(err));
+
+
+
   return (
     <div>
-     <Header />
-     <Footer />
-     
+    
+     <Routes>
+      <Route path='/Home' element={<Home/>}></Route>
+      <Route path='/Signup' element={<SignUp/>}></Route>
+      <Route path='/Login' element={<Login/>}></Route>
+     </Routes>
+    
     </div>
   );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer';
+import Header from './Header';
+function Home() {
+  return (
+    <div>
+         <Header />
+         <Footer />
+    </div>
+  )
+}
+
+export default Home
