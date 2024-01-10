0f617a5735cc6f75444a12ebd01f281b21c91902
mohdhadi01 <hadi.88923@gmail.com>
2024-01-11 04:05:14 +0530

    #completed Building the Carousel with all the details

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './Login & Signup/Login';
-import SignUp from './Login & Signup/SignUp';
-import Home from './Pages/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./Login & Signup/Login";
+import SignUp from "./Login & Signup/SignUp";
+import Home from "./Pages/Home";
+import { useEffect, useState } from "react";
 
 function App() {
   // const APIKey="a122cee36b1bc254ee171ee36a29bb98";
+  const [movieslist, setMoviesList] = useState([]);
 
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTIyY2VlMzZiMWJjMjU0ZWUxNzFlZTM2YTI5YmI5OCIsInN1YiI6IjY1MjgyY2E5MWYzZTYwMDEzOTlkMjg2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AHxTYhk6c-0aPLETLVS2-oxQqETUReQ5Uyhi9-eZ5bA'
-  //   }
-  // };
-  
-  // fetch('https://api.themoviedb.org/3/configuration', options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
-
+  async function fetchAPI() {
+    try {
+      // https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=a122cee36b1bc254ee171ee36a29bb98
+      // const response = await fetch(`https://api.themoviedb.org/3/movie/157336?api_key=${APIKey}&append_to_response=videos,images`)
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=Avengers&api_key=a122cee36b1bc254ee171ee36a29bb98`
+      );
+      const jsonData = await response.json();
+      setMoviesList(jsonData.results);
+      // console.log(movieslist);
+    } catch (e) {
+      console.log(e, "api error occured");
+    }
+  }
 
   return (
     <div>
-    
-     <Routes>
-      <Route path='/Home' element={<Home/>}></Route>
-      <Route path='/Signup' element={<SignUp/>}></Route>
-      <Route path='/Login' element={<Login/>}></Route>
-     </Routes>
-    
+      <Routes>
+        <Route path="/Home" element={<Home movieslist={movieslist} />}></Route>
+        <Route path="/Signup" element={<SignUp />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
